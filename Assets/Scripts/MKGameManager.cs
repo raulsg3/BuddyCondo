dff7a58f068d4be1383e16e4a7dd3e037359057e
raulsg3 <raulseguragaleote@gmail.com>
2019-01-26 00:58:22 +0100

    Base MKGameManager. Falta unir con la info definida en MKCellData

--- Assets/Scripts/MKGameManager.cs
+++ Assets/Scripts/MKGameManager.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class MKGameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Nombre de jugadores
+    //Tiempo
+    //Nivel actual
+
+    //Tipos de casillas y contenido
+    private enum EMKTileType
     {
-        
+        Empty,
+        Character,
+        Obstacle,
+        Container,
+        Object
     }
 
-    // Update is called once per frame
-    void Update()
+    //Colores de personajes y objetos
+    private enum EMKColor
     {
-        
+        None,
+        Black,
+        Blue,
+        Red,
+        Yellow
+    }
+
+    //Typedef TILE
+    private struct MKTile
+    {
+        public uint row;
+        public uint col;
+        public EMKTileType type;
+    }
+
+    //Grid del juego
+    private MKTile[,] m_grid;
+
+    void Start()
+    {
+        uint gridWidth = MKGame.Instance.GetGameContent().GetGameManagerContent().m_gridWidth;
+        uint gridHeight = MKGame.Instance.GetGameContent().GetGameManagerContent().m_gridHeight;
+
+        m_grid = new MKTile[gridWidth, gridHeight];
+
+        for (uint r = 0; r < gridWidth; ++r)
+        {
+            for (uint c = 0; c < gridHeight; ++c)
+            {
+                m_grid[r, c].row = r;
+                m_grid[r, c].col = c;
+                m_grid[r, c].type = EMKTileType.Empty;
+            }
+        }
+    }
+
+    public Vector3 GetWorldPosition(Vector2 logicPosition)
+    {
+        return new Vector3(0, 0, 0);
     }
 }
---
+++ Assets/Scripts/MKGameManagerContent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SOGameManagerContent", menuName = "MK/SOGameManagerContent", order = 2)]
+public class MKGameManagerContent : ScriptableObject
+{
+    public uint m_gridWidth  = 40;
+    public uint m_gridHeight = 20;
+}
