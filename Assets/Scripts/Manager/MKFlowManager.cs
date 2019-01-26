07934de5795e70e4ce4360f4dc1e264b2a7b6fa5
KaitoKid11 <daleorei@gmail.com>
2019-01-26 13:52:40 +0100

    MKGame / Added FlowManager and LevelLoader MKFlowManager / Added

---
+++ Assets/Scripts/Manager/MKFlowManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MKFlowManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_State = new State();
+        m_MKLevelLoader = MKGame.Instance.GetLevelLoader();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeLevel()
+    {
+        int level = Random.Range(1,1); //1 FOR DEBUG PORPOUSES
+        m_MKLevelLoader.LoadLevel(level);
+        if (m_State.GameStatus != (int)Status.Game)
+        {
+            m_State.GameStatus = (int)Status.Game;
+        }
+    }
+
+    public void GameOver()
+    {
+        
+    }
+
+    internal class State
+    {
+        int currentStatus;
+
+        public State()
+        {
+            this.currentStatus = (int)Status.Menu;
+        }
+
+        public int GameStatus
+        {
+            get { return currentStatus; }
+            set { this.currentStatus = value; }
+        }
+    }
+
+    enum Status { Menu, Game, GameOver }
+    State m_State;
+    MKLevelLoader m_MKLevelLoader;
+}
