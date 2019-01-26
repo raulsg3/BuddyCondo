6f43d7c4c3f74ac08719c14fae57346807b7e472
raulsg3 <raulseguragaleote@gmail.com>
2019-01-26 12:48:42 +0100

    GameManager movement and placement

--- Assets/Scripts/MKCharacterController.cs
+++ Assets/Scripts/MKCharacterController.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class MKCharacterController : MonoBehaviour
 {
+    //Character movement
+    public enum EMKCharacterMove
+    {
+        Up = 0,
+        Right,
+        Bottom,
+        Left
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        MKRankingManager rankingManager = MKGame.Instance.GetRankingManager();
     }
 
     // Update is called once per frame
