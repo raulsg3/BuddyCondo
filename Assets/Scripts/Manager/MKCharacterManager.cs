9a52272af01b9c2fc522abb1dd291f1569f669b8
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2019-01-26 19:17:31 +0100

    charactermanager

---
+++ Assets/Scripts/Manager/MKCharacterManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MKCharacterManager : MonoBehaviour
+{
+  public MKCharacterController player1;
+  public MKCharacterController player2;
+
+  public void SetPlayerActiveStatus(bool status){
+    player1.playerActive = status;
+    player2.playerActive = status;
+  }
+}
