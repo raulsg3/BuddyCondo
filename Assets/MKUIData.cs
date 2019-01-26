51d3dd465988d6eb880e250130ae4718febfdcc0
KaitoKid11 <daleorei@gmail.com>
2019-01-26 18:46:26 +0100

    MKUIData / Added UIPrefabs / Added FlowManager / Management between LoadLevel and GameManager

---
+++ Assets/MKUIData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MKUIData : MonoBehaviour
+{
+    public GameObject rankingUI;
+    public GameObject victoryUI;
+    public GameObject startingUI;
+
+    public GameObject GetUIGO(string uiElement)
+    {
+        switch (uiElement)
+        {
+            case "rankingUI":
+                return rankingUI;
+                break;
+            case "victoryUI":
+                return victoryUI;
+                break;
+            case "startingUI":
+                return startingUI;
+                break;
+            default:
+                return null;
+                break;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
