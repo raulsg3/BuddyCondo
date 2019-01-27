cf85888d2e21df348d69ca62bdfefcae47785d1d
KaitoKid11 <daleorei@gmail.com>
2019-01-27 14:38:04 +0100

    El tag caraho

--- Assets/Scripts/Manager/MKFlowManager.cs
+++ Assets/Scripts/Manager/MKFlowManager.cs
@@ -46,7 +46,7 @@ public class MKFlowManager : MonoBehaviour
 
     public void LoadLevel(uint level)
     {
-        MKGame.Instance.GetUIManager().UpdateLevelText(level);
+        MKGame.Instance.GetUIManager().UpdateLevelText(level.ToString());
         m_State.GameStatus = (int)Status.LoadingLevel;
         m_MKLevelLoader.LoadLevel(level.ToString());
     }
